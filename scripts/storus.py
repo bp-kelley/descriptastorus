0aa7c732082ab655a99edd17624662f7aadf4ee5
Brian Kelley <brian.kelley@novartis.com>
2017-01-30 16:05:13 -0500

    Uses make_store implementation

--- scripts/storus.py
+++ scripts/storus.py
@@ -1,214 +1,44 @@
-#!/bin/env python
+#!/usr/bin/env python
 """storus
 see Description below
 """
 from __future__ import print_function
-from descriptastorus.descriptors import MakeGenerator
-from rdkit.Chem import AllChem
+from descriptastorus import make_store
+import argparse, os, shutil, time
 
 import sys
 from rdkit import rdBase
 rdBase.DisableLog("rdApp.*")
 
-props = []
 
-def process( job ):
-    res = []
-    for index,smiles in job:
-        counts = props[0].process(smiles)
-        if not counts:
-            continue
-        res.append((index,counts))
+parser = argparse.ArgumentParser()
+parser.add_argument("smilesfile",
+                    help="file containing smiles strings")
+parser.add_argument("storage",
+                    help="directory in which to store the descriptors")
 
-    return res
+parser.add_argument("--descriptors", default="Morgan3Counts,RDKit2D")
+parser.add_argument("--hasHeader", action="store_true",
+                    help="Indicate whether the smiles file has a header row")
 
-def processInchi( job ):
-    res = []
-    try:
-        mols = []
-        allSmiles = []
-        indices = []
-        for index, smiles in job:
-            try:
-                m = AllChem.MolFromSmiles(smiles)
-                if not m: continue
-                
-                mols.append(m)
-                allSmiles.append(smiles)
-                indices.append(index)
-            except:
-                continue
+parser.add_argument("--index-inchikey", action="store_true",
+                    help="Optionally index the descriptors with inchi keys")
 
-        results = props[0].processMols(mols, allSmiles)
-        assert(len(results) == len(mols))
-        for index, result, m in zip(indices, results, mols):
-            if not result:
-                continue
-            inchi = AllChem.MolToInchi(m)
-            key = AllChem.InchiToInchiKey(inchi)
-#            assert ('None' not in result)
-            res.append((index, result, inchi, key))
-            
-    except Exception, x:
-        import traceback
-        traceback.print_exc()
+#parser.add_argument("--index-smiles", action="store_true",
+#                    help="Indicate whether the smiles file has a header column")
 
-    return res
+parser.add_argument("--smilesColumn", default=0,
+                    help="Row index (or header name if the file has a header) for the smiles column")
+parser.add_argument("--nameColumn", default=-1,
+                    help="Row index (or header name if the file has a header) for the name column")
+
+parser.add_argument("--seperator", default="\t",
+                    help="Row index (or header name if the file has a header) for the name column")
 
 
+opts = parser.parse_args()
+make_store.make_store(opts)
 
-if __name__ == "__main__":
-
-    import argparse, os, shutil, time
-    from descriptastorus import MolFileIndex, raw
-    try:
-        import kyotocabinet
-    except:
-        kyotocabinet = None
-        
-    parser = argparse.ArgumentParser()
-    parser.add_argument("smilesfile",
-                        help="file containing smiles strings")
-    parser.add_argument("storage",
-                        help="directory in which to store the descriptors")
-
-    parser.add_argument("--descriptors", default="Morgan3Counts,RDKit2D")
-    parser.add_argument("--hasHeader", action="store_true",
-                        help="Indicate whether the smiles file has a header row")
-
-    parser.add_argument("--index-inchikey", action="store_true",
-                        help="Optionally index the descriptors with inchi keys")
-
-    #parser.add_argument("--index-smiles", action="store_true",
-    #                    help="Indicate whether the smiles file has a header column")
-    
-    parser.add_argument("--smilesColumn", default=0,
-                        help="Row index (or header name if the file has a header) for the smiles column")
-    parser.add_argument("--nameColumn", default=-1,
-                        help="Row index (or header name if the file has a header) for the name column")
-    
-    parser.add_argument("--seperator", default="\t",
-                        help="Row index (or header name if the file has a header) for the name column")
-    
-
-    args = parser.parse_args()
-
-    props.append( MakeGenerator(args.descriptors.split(",")) )
-    properties = props[0]
-
-    inchiKey = args.index_inchikey
-    if inchiKey and not kyotocabinet:
-        print("Indexing inchikeys requires kyotocabinet, please install kyotocabinet",
-              file=sys.stderr)
-        sys.exit(1)
-    
-    # make the storage directory
-    if os.path.exists(args.storage):
-        raise IOError("Directory for descriptastorus already exists: %s"%args.storage)
-    
-    os.mkdir(args.storage)
-    # index the molfile
-    indexdir = os.path.join(args.storage, "__molindex__")
-
-    sm = MolFileIndex.MakeSmilesIndex(args.smilesfile, indexdir,
-                                      sep=args.seperator,
-                                      hasHeader = args.hasHeader,
-                                      smilesColumn = args.smilesColumn,
-                                      nameColumn = args.nameColumn)
-    print("Creating empty descriptor storage for %s molecules..."%sm.N)
-
-                                      
-    import multiprocessing
-    import time, os, sys, numpy
-        
-    numstructs = sm.N
-    s = raw.MakeStore(properties.GetColumns(), sm.N, args.storage,
-                      checkDirectoryExists=False)
-    if args.index_inchikey:
-        cabinet = kyotocabinet.DB()
-        inchi = os.path.join(args.storage, "inchikey.kch")
-        cabinet.open(inchi, kyotocabinet.DB.OWRITER | kyotocabinet.DB.OCREATE)
-
-    if args.nameColumn != -1:
-        name_cabinet = kyotocabinet.DB()
-        name = os.path.join(args.storage, "name.kch")
-        name_cabinet.open(name, kyotocabinet.DB.OWRITER | kyotocabinet.DB.OCREATE)
-        
-        
-    num_cpus = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(num_cpus)
-    print ("Number of molecules to process", numstructs)
-    
-    done = False
-    count = 0
-    batchsize = 500
-    badColumnWarning = False
-    inchies = {}
-    while 1:
-        lastcount = count
-        joblist = []
-        for cpuidx in range(num_cpus):
-            jobs = []
-            for i in range(count, min(count+batchsize, sm.N)):
-                jobs.append((i,sm.getMol(i)))
-            count = i+1
-            if jobs:
-                joblist.append(jobs)
-        if not joblist:
-            break
-        t1=time.time()
-
-        if args.index_inchikey:
-            results = pool.map(processInchi, joblist)
-        else:
-            results = pool.map(process, joblist)
-            
-        print("#jobs:", len(joblist), "#results:", len(results),
-              file=sys.stderr)
-        for result in results:
-            if not badColumnWarning and len(result) == 0:
-                badColumnWarning = True
-                print("WARNING: no molecules processed in batch, check the smilesColumn",
-                      file=sys.stderr)
-                print("WARNING: First 10 smiles:\n",
-                      file=sys.stderr)
-                print("\n".join([sm.getMol(i) for i in range(0, min(sm.N,10))]),
-                      file=sys.stderr)
-
-            if args.index_inchikey:
-                for i,v,inchi,key in result:
-                    try:
-                        s.putRow(i, v)
-                    except:
-                        print("Index %s row %s"%(i,v), file=sys.stderr)
-                        raise
-                    
-                    if inchi in inchies:
-                        inchies[key].append(i)
-                    else:
-                        inchies[key] = [i]
-
-                    if args.nameColumn != -1:
-                        name = sm.getName(i)
-                        name_cabinet[name] = i
-                
-            else:
-                for i,v in result:
-                    s.putRow(i, v)
-                    name = sm.getName(i)
-                    if name in name_cabinet:
-                        print("WARNING: name %s duplicated at molecule %d and %d"%(
-                            name, name_cabinet[name], i))
-                    else:
-                        name_cabinet[name] = i
-        t2=time.time()        
-        print("Done with %s out of %s - %s seconds"%(count, sm.N, t2-t1), file=sys.stderr)
-
-    if args.index_inchikey:
-        print("Indexing inchies", file=sys.stderr)
-        for k in sorted(inchies):
-            cabinet[k] = repr(inchies[k])
-    
 
         
             
