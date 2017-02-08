ee44cc0592cf73bfd7f3722bc1fb9b53e32ad8b2
Brian Kelley <brian.kelley@novartis.com>
2017-02-08 13:58:42 -0500

    Adds better checks

--- scripts/check-compatible.py
+++ scripts/check-compatible.py
@@ -23,14 +23,21 @@ store2 = DescriptaStore(opts.storage2)
 
 if store1.getDescriptorNames() != store2.getDescriptorNames():
     logging.warning("Storages are not compatible, columns are different")
-    if set(store1.getDescriptorNames()) == set(store2.getDescriptorNames()):
+    s1 = set(store1.getDescriptorNames())
+    s2 = set(store2.getDescriptorNames())
+    
+    if s1 == s2:
         logging.warning("Columns are the same but reordered")
+    else:
+        logging.warning("Extra columns in store1:\n%s", "\n\t".join(s1-s2))
+        logging.warning("Extra columns in store2:\n%s", "\n\t".join(s2-s1))
         
-    for a,b in zip(store1.getDescriptorNames(),
-                   store2.getDescriptorNames()):
+    for i,(a,b) in enumerate(zip(store1.getDescriptorNames(),
+                                 store2.getDescriptorNames())):
         if a != b:
-            logging.warning("First differing element %s != %s", a,b)
-            
+            logging.warning("First differing element at index %s (%s != %s)", i,a,b)
+            break
+        
     sys.exit(1)
     
 # check stores...    
