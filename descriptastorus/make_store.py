9ee0bfb4a6c6947daf73fc1054b16e78928918d3
Brian Kelley <brian.kelley@novartis.com>
2018-06-27 15:50:29 -0400

    Add warning for not storing inch/names

--- descriptastorus/make_store.py
+++ descriptastorus/make_store.py
@@ -189,12 +189,16 @@ def make_store(options):
             cabinet = kyotocabinet.DB()
             inchi = os.path.join(options.storage, "inchikey.kch")
             cabinet.open(inchi, kyotocabinet.DB.OWRITER | kyotocabinet.DB.OCREATE)
+        else:
+            logging.warning("Not logging inchi (see --index-inchkey)")
 
         if options.nameColumn is not None:
             logging.info("Creating name store")
             name_cabinet = kyotocabinet.DB()
             name = os.path.join(options.storage, "name.kch")
             name_cabinet.open(name, kyotocabinet.DB.OWRITER | kyotocabinet.DB.OCREATE)
+        else:
+            logging.warning("Not storing name lookup (see --nameColumn)")
 
 
         logging.info("Number of molecules to process: %s", numstructs)
