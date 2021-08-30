20ca43c085c10ccdafacef7ab25bba46c95da034
Brian Kelley <fustigator@gmail.com>
2021-08-30 16:58:48 -0400

    Remove Warnings

--- descriptastorus/descriptors/rdNormalizedDescriptors.py
+++ descriptastorus/descriptors/rdNormalizedDescriptors.py
@@ -51,9 +51,9 @@ for name, (dist, params, minV,maxV,avg,std) in dists.dists.items():
     
     cdfs[name] = cdf
 
-for name in rdDescriptors.FUNCS:
-    if name not in cdfs:
-        logging.warning("No normalization for %s", name)
+#for name in rdDescriptors.FUNCS:
+#    if name not in cdfs:
+#        logging.warning("No normalization for %s", name)
 
 def applyNormalizedFunc(name, m):
     if name not in cdfs:
