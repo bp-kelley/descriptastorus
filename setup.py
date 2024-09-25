3f7626ecdc7582df5a148600aa683e4bd768d216
Brian Kelley <fustigator@gmail.com>
2024-09-25 13:18:54 -0400

    Fix email (#34)

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     version=VERSION,
     description='Descriptor creation, storage and molecular file indexing',
     author='Brian Kelley',
-    author_email='bkelley@relaytx.com',
+    author_email='fustigator+descriptastorus@gmail.com',
     url='https://github/bp-kelley/descriptastorus',
     install_requires=['pandas_flavor', 'rdkit', 'scipy', 'numpy'],
     test_suite='nose.collector',
